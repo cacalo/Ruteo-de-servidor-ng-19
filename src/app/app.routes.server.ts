060e55d4cb2abc3a27e4bdbba0823d07a5ff4bc3
Gonzalo <cacalo219@hotmail.com>
2025-01-16 10:02:41 -0300

    New: Agregado status code 404 para el not found

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -12,6 +12,11 @@ export const serverRoutes: ServerRoute[] = [
     }
   },
   {
+    path: "not-found",
+    renderMode: RenderMode.Server,
+    status: 404
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
